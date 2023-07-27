5e09f279804efcc890eaf8e7c30ff4a8874aab42
Swathi <swathisak16@gmail.com>
2023-07-27 15:50:05 -0600

    daysinmonth2

---
+++ dayInMonth2.ts
@@ -0,0 +1,31 @@
+let monthNum1 : number = 2;
+let isLeapYear1 : boolean = true ;
+
+
+if(monthNum1==1){
+    console.log("Number of Days in January is 31")
+}else if(monthNum1==2){
+    console.log("Number of Days in Feburary", isLeapYear1 ? 28:29)
+}else if(monthNum1==3){
+    console.log("Number of Days in March is 31")
+}else if(monthNum1==4){
+    console.log("Number of Days in April is 30")
+}else if(monthNum1==5){
+    console.log("Number of Days in May is 31")
+}else if(monthNum1==6){
+    console.log("Number of Days in June is 30")
+}else if(monthNum1==7){
+    console.log("Number of Days in July is 31")
+}else if(monthNum1==8){
+    console.log("Number of Days in August is 31")
+}else if(monthNum1==9){
+    console.log("Number of Days in September is 30")
+}else if(monthNum1==10){
+    console.log("Number of Days in October is 31")
+}else if(monthNum1==11){
+    console.log("Number of Days in November is 30")
+}else if(monthNum1==12){
+    console.log("Number of Days in December is 31")
+}else{
+    console.log("Incorrect Month. Please enter between 1 and 12")
+}
